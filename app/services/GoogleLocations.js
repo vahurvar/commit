55dfb6258ba1bf9d0a27b7520b6b68fed9253ffd
varrisvahur@gmail.com <varrisvahur@gmail.com>
2017-07-19 16:39:28 +0200

    Added check gym nearby service

---
+++ app/services/GoogleLocations.js
@@ -0,0 +1,29 @@
+
+import * as querystring from "query-string";
+
+export default class GoogleLocations {
+
+    static async isGymNearby(latitude, longitude) {
+        try {
+            let response = await fetch(GoogleLocations.getUrl(latitude, longitude));
+            let responseJson = await response.json();
+            return responseJson.status === 'OK';
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    static getUrl(latitude, longitude) {
+        let apiUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+        let params = querystring.stringify({
+            key: 'AIzaSyAD1IVgtb98549HrYkr80p4sH8FtknzrZQ',
+            location: latitude + ',' + longitude,
+            type: 'gym',
+            radius: 250
+        });
+        return apiUrl + params;
+    }
+}
+
+
+
